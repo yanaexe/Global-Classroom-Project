bbab04a252d26187b0e7dec281c92736a1c7fd4a
Jana <janrheiasy@gmail.com>
2025-02-21 13:49:35 +0900

    added platform tiles, changed gd script,

--- scripts/character_player.gd
+++ scripts/character_player.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	var input_direction = Input.get_axis("left", "right")
+	velocity = Vector2(input_direction * speed, 0)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
