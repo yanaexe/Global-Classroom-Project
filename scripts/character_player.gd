7b402b9c1adf87e65494e89304519fadb1a5f8e4
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2025-02-26 02:40:35 +0000

    Jump mechanic implemented fully, player cannot spam jump. they gradually fall. Still need to figure out how to make it a smooth way up

--- scripts/character_player.gd
+++ scripts/character_player.gd
@@ -1,34 +1,38 @@
 extends CharacterBody2D
 
 @export var speed = 400
-@export var gravity = 980
+@export var gravity = 100 #adjust this to make the player get pulled to the ground faster
 @onready var animation = $AnimationPlayer
 var last_direction = "right"
 
+
 func get_input():
+	velocity.y += gravity #this constantly makes the character always pull down to ground
 	var input_direction = Input.get_axis("left", "right")
 	velocity = Vector2(input_direction * speed, 0)
+	
 
 	# animation handling
 	if input_direction > 0:
+		velocity.y += gravity
 		animation.play("walk_right")
 		last_direction = "right"
 	elif input_direction < 0:
+		velocity.y += gravity
 		animation.play("walk_left")
 		last_direction = "left"
 	else:
 		animation.play("idle_" + last_direction)
+		velocity.y= gravity
 		
 	# Jumping 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -500 
-		#animation.play("jump_" + last_direction) #Plays jump animation
-		print("Jump Triggered!")  # Debugging
+	if Input.is_action_just_pressed("jump") :
+		if is_on_floor():
+			velocity.y = -5000 
+			animation.play("jump_" + last_direction) #Plays jump animation
+			print("Jump Triggered!")  # Debugging
 
 func _physics_process(_delta):
-	#apply gravity
-	if not is_on_floor():
-		velocity.y += gravity * _delta # This applies gravity only when the character is not on the ground
 	get_input()
 	move_and_slide()
 	
