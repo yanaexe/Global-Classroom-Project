a88c039cb47021b0082e00a4088e5f74e9069d6b
C22374581 <C22374581@mytudublin.ie>
2025-03-12 14:25:25 +0000

    Added dash ability to player movement

--- scripts/character_player.gd
+++ scripts/character_player.gd
@@ -1,20 +1,30 @@
 extends CharacterBody2D
 
 @export var speed = 300
-@export var gravity = 1500  #adjust this to make the player get pulled to the ground faster
+@export var gravity = 1500  # Adjust this to make the player get pulled to the ground faster
 @export var jump_strength = 400  
+@export var dash_speed = 600  # Speed boost during dash
+@export var dash_duration = 0.2  # Dash time in seconds
+@export var dash_cooldown = 1.0  # Cooldown before dashing again
+
 @onready var animation = $AnimationPlayer
 var last_direction = "right"
+var is_dashing = false
+var dash_timer = 0.0
+var dash_cooldown_timer = 0.0
 
 func get_input():
 	var input_direction = Input.get_axis("left", "right")
-	velocity.x = input_direction * speed
-	
+
+	# Apply normal movement if not dashing
+	if not is_dashing:
+		velocity.x = input_direction * speed
+
 	if not is_on_floor():
 		velocity.y += gravity * get_process_delta_time()  # Apply gravity only when not on the floor
 	else:
 		velocity.y = 0  # Reset vertical velocity to 0 when on the ground
-	
+
 	# Animation handling
 	if input_direction > 0:
 		animation.play("walk_right")
@@ -30,10 +40,27 @@ func get_input():
 		velocity.y = -jump_strength  # Apply jump force
 		animation.play("jump_" + last_direction)
 
-func _physics_process(_delta):
+	# Dash Mechanic
+	if Input.is_action_just_pressed("dash") and not is_dashing and dash_cooldown_timer <= 0:
+		is_dashing = true
+		dash_timer = dash_duration
+		dash_cooldown_timer = dash_cooldown
+		velocity.x = dash_speed if last_direction == "right" else -dash_speed
+		animation.play("dash_" + last_direction)
+
+func _physics_process(delta):
+	# Handle dashing
+	if is_dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing = false  # End dash
+
+	# Cooldown countdown
+	if dash_cooldown_timer > 0:
+		dash_cooldown_timer -= delta
+
 	get_input()
 	move_and_slide() 
-	
 
 func die():
 	if last_direction == "right":
