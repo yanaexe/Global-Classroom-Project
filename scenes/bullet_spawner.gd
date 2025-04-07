580809fe63626772c07efd400f0cb5309f19effd
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2025-04-07 09:32:29 +0100

    Dodge minigame bullets spawn randomly in box and shoot randomly

--- scenes/bullet_spawner.gd
+++ scenes/bullet_spawner.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var bullet_scene: PackedScene = preload("res://scenes/dodge_bullet.tscn")
 @export var spawn_interval: float = 0.5  # Seconds between shots
 
+@onready var area := get_node("/root/DodgeMinigame/BattleBox/Border/BattleArea")
+
 var spawn_timer := 0.0
 
 func _process(delta):
@@ -12,26 +14,25 @@ func _process(delta):
 		spawn_timer = spawn_interval
 
 func spawn_bullet():
-	var player = get_tree().get_nodes_in_group("player_soul")[0]
-	var area = get_node("/root/DodgeMinigame/BattleBox/Border/BattleArea")
-	
-	var global_top_left = area.get_global_transform_with_canvas().origin
-	var size = area.get_size()  # ColorRect's size in world coords
+	# Get global rectangle of the area (for Control nodes like ColorRect)
+	var rect = area.get_global_rect()
 
-	var margin = 0
-	var min_x = global_top_left.x + margin
-	var max_x = global_top_left.x + size.x - margin
-	var min_y = global_top_left.y + margin
-	var max_y = global_top_left.y + size.y - margin
+	# Generate a random position inside that rectangle
+	var spawn_pos = Vector2(
+		randf_range(rect.position.x, rect.position.x + rect.size.x),
+		randf_range(rect.position.y, rect.position.y + rect.size.y)
+	)
 
-	global_position = Vector2(randf_range(min_x, max_x), randf_range(min_y, max_y))
-	print("🔫 FIXED Spawner Pos:", global_position)
-
+	# Create the bullet
 	var bullet = bullet_scene.instantiate()
-	bullet.global_position = global_position
+	bullet.global_position = spawn_pos
 	get_tree().get_root().add_child(bullet)
 
-	var dir = player.global_position - bullet.global_position
+	# Find the player
+	var player = get_tree().get_nodes_in_group("player_soul")[0]
+
+	# Aim the bullet at the player
+	var dir = player.global_position - spawn_pos
 	if dir.length() < 2:
 		dir = Vector2.DOWN
 	bullet.direction = dir.normalized()
