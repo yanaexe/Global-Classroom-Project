6b2039bc7c4c01af6c9be0a308328e935eeb6ff0
Jana <janrheiasy@gmail.com>
2025-04-08 14:52:38 +0900

    Fixed the timer so the bullets and player stop after 5 seconds

--- scripts/dodge_minigame.gd
+++ scripts/dodge_minigame.gd
@@ -5,11 +5,14 @@ func _ready():
 
 func _on_Timer_timeout():
 	print("You survived!")
-	
+	var bullet_spawner = get_node("BattleBox/BulletSpawner")
 	# Stop spawning bullets (if you made a spawner node)
-	if has_node("BulletSpawner"):
-		$BulletSpawner.queue_free()
+	if bullet_spawner:
+		print("I'n here!")
+		bullet_spawner.set_process(false)
+		bullet_spawner.queue_free()
 	
+	print("I Didn't go through bullet spawner")
 	# Optional: freeze player, show message, etc.
 	var player = get_tree().get_nodes_in_group("player_soul")[0]
 	player.set_physics_process(false)
