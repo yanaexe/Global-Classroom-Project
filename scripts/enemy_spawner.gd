54a5ddaaa263e86a647e84d9f1c80d1224ba740a
cheomji <0311bom@naver.com>
2025-04-09 11:37:33 +0100

    added EnemySpawner node to stage1

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var slime_scene: PackedScene
 @export var spawn_count: int = 3
-@export var spawn_area: Rect2 = Rect2(Vector2(0, 0), Vector2(50, 50)) # adjust this to fit your stage
+@export var spawn_area: Rect2 = Rect2(Vector2(-1025, 25), Vector2(2499, 109)) # adjust this to fit your stage
 
 func _process(delta):
 	if Input.is_action_just_pressed("spawn_key"):
@@ -21,9 +21,12 @@ func _ready():
 			spawn_slime()
 	else:
 		print("❌ Slime scene not assigned!")
+		
+	await get_tree().process_frame
+	print("👾 TOTAL slimes in scene after delay:", get_tree().get_nodes_in_group("enemy").size())
+
 
 func spawn_slime():
-	
 	var slime = slime_scene.instantiate()
 	print("👀 Slime children:", slime.get_children())
 	print("✅ Slime instantiated as:", slime.get_class())
@@ -38,11 +41,18 @@ func spawn_slime():
 	debug_sprite.position = Vector2.ZERO  # ensure it's centered
 	slime.add_child(debug_sprite)
 
-	slime.global_position = Vector2(400, 300)  # hardcoded test pos
+	#slime.global_position = Vector2(400, 300)  # hardcoded test pos
+	var spawn_pos = Vector2(
+	randf_range(spawn_area.position.x, spawn_area.position.x + spawn_area.size.x),
+	randf_range(spawn_area.position.y, spawn_area.position.y + spawn_area.size.y))
+	#slime.global_position = spawn_pos
+	slime.position = spawn_pos
+
 	get_tree().current_scene.add_child(slime)
-
-	print("✅ Slime added to scene at 0,0")
-
-
-	get_tree().current_scene.add_child(slime)
-	print("✅ Slime added to scene")
+	slime.add_to_group("enemy")
+	
+	print("👾 Spawned slime at:", slime.position)
+	print("🐾 Spawning slime at:", spawn_pos)
+	print("👾 Total slimes in scene:", get_tree().get_nodes_in_group("enemy").size())
+	#get_tree().current_scene.add_child(slime)
+	#print("✅ Slime added to scene")
