3ef10a1bc0d21cf6c18ed8d3870d486c1942c5ab
Jana <janrheiasy@gmail.com>
2025-04-10 23:38:37 +0900

    basic try again menu started

---
+++ escape_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_try_again_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/stage_1.tscn")
+
+
+func _on_home_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
